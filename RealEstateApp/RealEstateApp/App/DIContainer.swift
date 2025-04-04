4c9b678239e465c24d1b7caa0d7d423216526582
David Lopez <quindos00@gmail.com>
2025-04-04 22:43:00 +0200

    Init app

---
+++ RealEstateApp/RealEstateApp/App/DIContainer.swift
@@ -0,0 +1,8 @@
+//
+//  DIContainer.swift
+//  RealEstateApp
+//
+//  Created by David López on 4/4/25.
+//
+
+
---
+++ RealEstateApp/RealEstateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealEstateApp
+//
+//  Created by David López on 4/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
